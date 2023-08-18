83d83ea192aaa7ee9ec1e2f38fdadf11665a3c14
msra <msra@h100pv2.guest.corp.microsoft.com>
2023-08-18 08:33:58 +0000

    new repo cutlass-mod

--- examples/99_kernel_test/99_kernel_test.cu
+++ examples/99_kernel_test/99_kernel_test.cu
@@ -103,7 +103,7 @@ using ElementAccumulator  = float;                                          // E
 using ArchTag             = cutlass::arch::Sm90;                            // Tag indicating the minimum SM that supports the intended feature
 using OperatorClass       = cutlass::arch::OpClassTensorOp;                 // Operator class tag
 using TileShape           = Shape<_128,_256,_64>;                           // Threadblock-level tile size
-using ClusterShape        = Shape<_1,_1,_1>;                                // Shape of the threadblocks in a cluster
+using ClusterShape        = Shape<_1,_2,_1>;                                // Shape of the threadblocks in a cluster
 using StageCountType = cutlass::gemm::collective::StageCountAuto;           // Stage count maximized based on the tile size
 using KernelSchedule = cutlass::gemm::collective::KernelScheduleAuto;       // Kernel to launch based on the default setting in the Collective Builder 
 
